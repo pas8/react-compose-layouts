ffc8f455a3acca9bae9a365cfcfb81b197c978e1
pas <whyiampas@gmail.com>
2021-07-31 16:34:06 +0300

    tryind to fix this shit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
+    globalObject: 'this',
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
     libraryTarget: 'umd' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
